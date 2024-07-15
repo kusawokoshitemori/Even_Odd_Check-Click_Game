e7b17a6fa94f5ee9c66c65ed351b160f957a4c44
草をこして森 <kyou0317kyou0317@gmail.com>
2024-07-15 19:15:32 +0900

    ゲームスタート押したら別画面に移動できるように

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ app.get("/", (req, res) => {
   res.render("top.ejs");
 });
 
+app.get("/game_page", (req, res) => {
+  res.render("game_page.ejs");
+});
+
 // サーバーを起動するコード
 app.listen(3000, () => {
   console.log("Server is running on http://localhost:3000");
