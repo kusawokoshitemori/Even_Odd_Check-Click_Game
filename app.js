03bbcd471d42f0d5727ac98963f376bd14ab9733
草をこして森 <kyou0317kyou0317@gmail.com>
2024-07-15 12:54:45 +0900

    一回手動でできるかどうか確認

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-//なんか書いておく
+const express = require("express");
+const app = express();
+
+// テンプレートエンジンとしてEJSを設定
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("top.ejs");
+});
+
+// サーバーを起動するコード
+app.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
+});
