581b556db9323e5bfa66fed80700042ca42cbdb9
草をこして森 <kyou0317kyou0317@gmail.com>
2024-07-16 01:52:50 +0900

    完成。満足

--- app.js
+++ app.js
@@ -15,6 +15,10 @@ app.get("/game_page", (req, res) => {
   res.render("game_page.ejs");
 });
 
+app.get("/game_rule", (req, res) => {
+  res.render("game_rule.ejs");
+});
+
 // サーバーを起動するコード
 app.listen(3000, () => {
   console.log("Server is running on http://localhost:3000");
