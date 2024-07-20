4274d8d2f4083c147b736bacac96ef41342e84cd
草をこして森 <kyou0317kyou0317@gmail.com>
2024-07-20 20:01:20 +0900

    名前を変更できるようにした

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 // プレイヤーネームをグローバルに管理
-let playerName = "Guest";
+let playerName = "ランキングから名前を入力してください";
 
 // テンプレートエンジンとしてEJSを設定
 app.set("view engine", "ejs");
@@ -13,19 +15,25 @@ app.set("views", "views");
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("top.ejs");
+  res.render("top.ejs", { playerName });
 });
 
 app.get("/game_page", (req, res) => {
-  res.render("game_page.ejs");
+  res.render("game_page.ejs", { playerName });
 });
 
 app.get("/game_rule", (req, res) => {
-  res.render("game_rule.ejs");
+  res.render("game_rule.ejs", { playerName });
 });
 
 app.get("/ranking", (req, res) => {
   res.render("ranking.ejs", { playerName });
+});
+
+app.post("/submit_username", (req, res) => {
+  playerName =
+    req.body.input_playerName.trim() || "ランキングから名前を入力してください"; // プレイヤーネームが空なら'Guest'
+  res.redirect("/"); // トップ画面にリダイレクト
 });
 
 // サーバーを起動するコード
