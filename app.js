0c7ecce20d9f4fbfcc498948216a4a7201463ca2
草をこして森 <kyou0317kyou0317@gmail.com>
2024-07-22 18:55:14 +0900

    MySQLにデータを入力できるようにするところまで行った

--- app.js
+++ app.js
@@ -5,12 +5,16 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// プレイヤーネームをグローバルに管理
+// プレイヤーネームとスコアをどこからでも使えるようにする
 let playerName = "ランキングから名前を入力してください";
+let now_game_score = 0;
 
 // テンプレートエンジンとしてEJSを設定
 app.set("view engine", "ejs");
 app.set("views", "views");
+
+// JSON ボディを解析するミドルウェア
+app.use(express.json());
 
 //connection poolを作成
 const pool = mysql.createPool({
@@ -52,6 +56,11 @@ app.get("/ranking", (req, res) => {
   res.render("ranking.ejs", { playerName });
 });
 
+//とりあえず入れたやつ。後で消してもいい
+app.get("/game_result", (req, res) => {
+  res.render("game_rusult.ejs");
+});
+
 //名前を変更するプログラム
 app.post("/submit_username", (req, res) => {
   playerName =
@@ -61,6 +70,26 @@ app.post("/submit_username", (req, res) => {
 
 app.get("/High_Score_ranking", (req, res) => {
   res.render("High_Score_ranking.ejs", { playerName });
+});
+
+app.post("/Game_Score_Send", (req, res) => {
+  console.log("Request body:", req.body);
+  const { PlayerName, Score } = req.body; // 変数名を一致させる
+  console.log("Received PlayerName:", PlayerName);
+  console.log("Received Score:", Score);
+
+  // MySQLの接続プールを使用してデータを挿入
+  pool.query(
+    "INSERT INTO score (PlayerName, Score) VALUES (?, ?)",
+    [PlayerName, Score], // ここも一致させる
+    (error, results) => {
+      if (error) {
+        console.error("Error inserting data:", error);
+        return res.status(500).send("Internal Server Error");
+      }
+      res.redirect("/gameresult.ejs"); // データ挿入後にリダイレクト
+    }
+  );
 });
 
 // サーバーを起動するコード
